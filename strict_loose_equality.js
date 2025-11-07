71a904db585106f7abc2c750863de5494d156c22
bmonagan <bernardmonagan@gmail.com>
2025-11-07 14:11:13 -0500

    strict_loose_equality

---
+++ strict_loose_equality.js
@@ -0,0 +1,18 @@
+// Given variables
+let a = 10
+let b = "10"
+let c = true
+
+// Type your code below
+
+let d = a == b
+let e = a === c
+let f = b !=c
+let g = a !== b
+
+
+// Don't change the line below
+console.log(`d = ${d}`)
+console.log(`e = ${e}`)
+console.log(`f = ${f}`)
+console.log(`g = ${g}`)
