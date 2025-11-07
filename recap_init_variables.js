162d3d20001bc16df70cd75d27c495693cf716fd
bmonagan <bernardmonagan@gmail.com>
2025-11-06 20:43:50 -0500

    recap_init_variables

---
+++ recap_init_variables.js
@@ -0,0 +1,8 @@
+// Type your code below
+let k = 88
+let PI = 3.14
+let name = "bob"
+// Don't change the line below
+console.log(`k = ${k}`)
+console.log(`PI = ${PI}`)
+console.log(`name = ${name}`)
