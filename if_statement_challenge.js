4027911fb299ce655693448bbfa7e103ddb69173
bmonagan <bernardmonagan@gmail.com>
2025-11-07 14:33:44 -0500

    if_statement_challenge

---
+++ if_statement_challenge.js
@@ -0,0 +1,15 @@
+let temperature = 25;
+let message = ""; // Set the message based on temperature
+
+// Add your if statements below
+if (temperature > 30){
+    message = "Too hot!"
+}
+if (temperature < 10){
+    message = "Too cold!"
+} 
+if (10 < temperature < 30){
+    message = "Just right!"
+}
+
+console.log(message); // This will show the result
