3fefb2605abb301be63ab7285288a3eb33660090
bmonagan <bernardmonagan@gmail.com>
2025-11-07 14:30:28 -0500

    type_coercion

---
+++ type_coercion.js
@@ -0,0 +1,18 @@
+// Given variables
+let num = 42
+let bool = false
+let str = "7"
+
+// Type your code below
+let a = String(num)
+let b = Number(bool)
+let c = Number(str)
+
+let d = num + str
+
+
+// Don't change the line below
+console.log(`a = ${a}`)
+console.log(`b = ${b}`)
+console.log(`c = ${c}`)
+console.log(`d = ${d}`)
