96714a822bc11960bd38bd7a61578065b47961a0
bmonagan <bernardmonagan@gmail.com>
2025-11-06 20:48:50 -0500

    modulo

---
+++ modulo.js
@@ -0,0 +1,17 @@
+// Type your code below
+let a = 9
+let b = 2.6
+let c = 11
+
+let d = a % 2
+let e = a % 3
+let f = c % 10
+
+
+// Don't change the line below
+console.log(`a = ${a}`)
+console.log(`b = ${b}`)
+console.log(`c = ${c}`)
+console.log(`d = ${d}`)
+console.log(`e = ${e}`)
+console.log(`f = ${f}`)
