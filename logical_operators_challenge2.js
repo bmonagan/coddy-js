2c88d5082b9cc485b364918ea3b02effbc37ccdb
bmonagan <bernardmonagan@gmail.com>
2025-11-07 14:30:36 -0500

    logical_operators_challenge2

---
+++ logical_operators_challenge2.js
@@ -0,0 +1,14 @@
+// Initialize variables
+let has_license = true
+let has_insurance = false
+let has_experience = true
+
+// Calculate conditions
+let can_drive_car = (has_license && has_insurance) || (has_license && has_experience)
+let can_drive_motorcycle = has_experience && has_insurance && has_license
+let cannot_drive_any = !has_license || (has_insurance && has_experience)
+
+// Don't delete the lines below
+console.log("Can drive car:", can_drive_car)
+console.log("Can drive motorcycle:", can_drive_motorcycle)
+console.log("Cannot drive any:", cannot_drive_any)
