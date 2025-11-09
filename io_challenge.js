f195231482dd18fcb98859d679af66c9a44abc58
bmonagan <bernardmonagan@gmail.com>
2025-11-09 00:03:58 -0500

    io_challenge

---
+++ io_challenge.js
@@ -0,0 +1,11 @@
+let rnd = inp; // Don't change this line
+let evenodd;
+
+if (rnd % 2 == 0){
+    evenodd = "The number is even."
+
+} else{
+    evenodd = "The number is odd."
+}
+
+console.log(`The input is: ${rnd}\n${evenodd}`)
