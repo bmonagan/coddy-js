88f107d10e47ed99380675b484131b395fcf8753
bmonagan <bernardmonagan@gmail.com>
2025-11-10 11:17:46 -0500

    return

---
+++ return.js
@@ -0,0 +1,9 @@
+let num = parseInt(inp); // Don't change this line
+// Type your code below
+
+function square(n){
+    let sq = n * n;
+    return sq;
+}
+result = square(num);
+console.log(result)
