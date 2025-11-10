a942bb4ad29d56c9a22bed728cd898899db42d5d
bmonagan <bernardmonagan@gmail.com>
2025-11-09 22:28:36 -0500

    welcome_message

---
+++ welcome_message.js
@@ -0,0 +1,7 @@
+let billAmount = parseFloat(inp[0]); // Don't change this line
+let tipPercentage = parseFloat(inp[1]); // Don't change this line
+let numPeople = parseInt(inp[2]); // Don't change this line
+
+// Type your code below
+
+console.log("Bill Split Calculator")
