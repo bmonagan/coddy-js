230af8bef702ed6cfcfccc906c1c6d5a9ad2f094
bmonagan <bernardmonagan@gmail.com>
2025-11-10 11:13:58 -0500

    arguments

---
+++ arguments.js
@@ -0,0 +1,10 @@
+let a = parseInt(inp[0]); // Don't change this line
+let b = parseInt(inp[1]); // Don't change this line
+// Type your code below
+
+function product_create(n1,n2){
+    let answer = n1 * n2;
+    console.log(answer);
+}
+
+product_create(a,b);
