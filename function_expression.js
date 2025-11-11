da017941ec1dcd89c250c7d8abcbb527a2b914de
bmonagan <bernardmonagan@gmail.com>
2025-11-11 16:31:30 -0500

    function_expression

---
+++ function_expression.js
@@ -0,0 +1,9 @@
+// Type your code below
+function calculateArea(width,height){
+    let area = width * height;
+    return area;
+}
+
+// Don't change the lines below
+console.log(calculateArea(5, 10));
+console.log(calculateArea(3, 7));
