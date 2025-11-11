33e2586b78e5bb4fef0efd7fa367154a41e3e386
bmonagan <bernardmonagan@gmail.com>
2025-11-11 16:34:58 -0500

    function_expr_challenge

---
+++ function_expr_challenge.js
@@ -0,0 +1,11 @@
+// Type your code below
+function calculateVolume(length,width,height){
+    let volume = length * width * height;
+    return volume;
+}
+
+// Don't change the lines below
+console.log(calculateVolume(3, 4, 5));
+console.log(calculateVolume(2, 2, 2));
+console.log(calculateVolume(1, 4, 2));
+console.log(calculateVolume(9, 8, 7));
