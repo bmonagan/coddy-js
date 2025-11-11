aa7e95f6d19d68e5d4f920d7680131e86650558f
bmonagan <bernardmonagan@gmail.com>
2025-11-11 16:37:37 -0500

    arrow_functions

---
+++ arrow_functions.js
@@ -0,0 +1,5 @@
+// Type your code below
+let calculateBMI = (weight,height) => weight / (height * height);
+// Don't change the lines below
+console.log(calculateBMI(70, 1.75));
+console.log(calculateBMI(80, 1.8));
