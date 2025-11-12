38cde6f5b8c7b5a3cba97146e8685fae66bd2c29
bmonagan <bernardmonagan@gmail.com>
2025-11-12 14:13:24 -0500

    fizzbuzz p1

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+let num = parseInt(inp); // Don't change this line
+console.log("Welcome to FizzBuzz!")
+// if (num % 3 == 0){
+//     if (num % 7 == 0){
+//         console.log("Fizzbuzz");
+//     }
+//     else{
+//         console.log("Fizz");
+//     }}
+// else if (num % 7 == 0){
+//     console.log("Buzz");
+// }
+// else{
+//     console.log(num);
+// }
+ 
+// Type your code below
