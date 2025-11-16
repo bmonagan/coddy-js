cc3bab9298e4301770f51333de1ab6d2a5c43918
bmonagan <bernardmonagan@gmail.com>
2025-11-15 22:07:21 -0500

    glist3

--- grocery_list.js
+++ grocery_list.js
@@ -4,6 +4,20 @@ function addItem(ingredient){
     groceryList.push(ingredient);
     console.log(`${ingredient} added to the grocery list.`);
 }
+function removeItem(ingredient){
+    let index = groceryList.indexOf(ingredient);
+
+    if (index >= 0){
+        groceryList.splice(index,1);
+        console.log(`${ingredient} removed from the grocery list.`);
+
+    }
+    else{
+        console.log(`${ingredient} is not in the grocery list.`)
+    }
+}
 addItem("Milk");
 addItem("Bread");
 addItem("Eggs");
+removeItem("Bread");
+removeItem("Cheese");
