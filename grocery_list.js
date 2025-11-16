a466eea8a0814820032dc08e4d1d5932aedc7896
bmonagan <bernardmonagan@gmail.com>
2025-11-15 22:02:59 -0500

    glist2

--- grocery_list.js
+++ grocery_list.js
@@ -1,1 +1,9 @@
-let ingredients = [];
+let groceryList = [];
+
+function addItem(ingredient){
+    groceryList.push(ingredient);
+    console.log(`${ingredient} added to the grocery list.`);
+}
+addItem("Milk");
+addItem("Bread");
+addItem("Eggs");
