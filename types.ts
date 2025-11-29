71fff8bfc1eb6382dccc3e38744d0e6c59c0fc2f
bmonagan <bernardmonagan@gmail.com>
2025-11-29 00:59:23 -0500

    types

---
+++ types.ts
@@ -0,0 +1,9 @@
+// TODO: Write your code here
+// Declare the three variables with explicit type annotations and values
+const productName: string = "Gaming Mouse";
+const price: number = 49.99;
+const inStock: boolean = true;
+// Then print them to the console
+console.log(productName);
+console.log(price);
+console.log(inStock);
