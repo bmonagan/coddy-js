b08808b062f2c9e240fcdfb3eb6648c6ba0de61e
bmonagan <bernardmonagan@gmail.com>
2025-11-29 23:55:46 -0500

    read_only

---
+++ excercises/read_only.ts
@@ -0,0 +1,9 @@
+// TODO: Write your code here
+
+let configValues: readonly string[] = ["production", "database","cache"];
+let ports: readonly number[] = [3000, 5432, 6379];
+let features: readonly boolean[] = [true,false,true];
+
+console.log(configValues);
+console.log(ports);
+console.log(features);
