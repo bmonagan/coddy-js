53c3be1e366ce453f47e0f2a00e58d4de49e064b
bmonagan <bernardmonagan@gmail.com>
2025-11-29 23:51:14 -0500

    type_arrays

---
+++ excercises/type_arrays.js
@@ -0,0 +1,10 @@
+// TODO: Write your code here
+// Create the three typed arrays as described in the challenge
+let scores: number[] =[85,92,78];
+let studentNames: string[] = ["Alice", "Bob", "Charlie"];
+let passsedExams: boolean[] = [true,false,true];
+
+// Print all three arrays to the console
+console.log(scores);
+console.log(studentNames);
+console.log(passsedExams);
