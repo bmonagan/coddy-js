1527825cb33a854338d7249b10ef6566ddb573cd
bmonagan <bernardmonagan@gmail.com>
2025-11-30 01:54:57 -0500

    tuples

---
+++ excercises/tuples.ts
@@ -0,0 +1,10 @@
+// TODO: Write your code here
+// Create the productInfo tuple with product name and price
+let productInfo: [string,number] = ["Gaming Mouse", 79];
+// Create the coordinates tuple with x and y positions
+let coordinates: [number,number] = [15,25];
+// Access and print each value as specified
+console.log(productInfo[0]);
+console.log(productInfo[1]);
+console.log(coordinates[0]);
+console.log(coordinates[1]);
