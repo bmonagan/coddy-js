30e137135b6be847d822fb421068c8a36ee60894
bmonagan <bernardmonagan@gmail.com>
2025-11-30 23:38:15 -0500

    readOnly_tuples

---
+++ readOnly_tuples.ts
@@ -0,0 +1,10 @@
+// TODO: Write your code here
+// Create the readonly tuples as described in the challenge
+let startPoint: readonly [number,number] = [0,0];
+let colorRGB: readonly [number,number,number] = [255,128,64];
+let userRecord: readonly [number, string, boolean] = [42, 'admin', true];
+
+// Print all three tuples
+console.log(startPoint);
+console.log(colorRGB);
+console.log(userRecord);
