dae987201c2a79aefb8378341c89f421f7ced242
bmonagan <bernardmonagan@gmail.com>
2025-11-30 23:40:46 -0500

    multi_dimension_arrays

---
+++ multi_dimension_arrays.ts
@@ -0,0 +1,20 @@
+// TODO: Write your code here
+// Create the gameGrid 2D array (3x3 matrix with numbers 1-9)
+let gameGrid: number[][] = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+// Create the chessBoard 2D array (2x2 matrix with string values)
+let chessBoard: string[][] = [
+    ['white','black'],
+    ['black','white']
+];
+
+// Access and print the element at second row, first column of gameGrid
+console.log(gameGrid[1][0]);
+
+
+// Access and print the element at first row, second column of chessBoard
+console.log(chessBoard[0][1]);
