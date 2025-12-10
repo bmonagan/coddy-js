c379ab670d6e92754c5668ed3612a9d31834bdba
bmonagan <bernardmonagan@gmail.com>
2025-12-10 01:07:03 -0500

    string enums

---
+++ excercises/string-enums.ts
@@ -0,0 +1,22 @@
+// TODO: Write your code here
+enum Direction {
+  Up = "UP",
+  Down = "DOWN", 
+  Left = "LEFT",
+  Right = "RIGHT"
+}
+
+// Print the required outputs
+
+const upDirection: Direction = Direction.Up;
+const downDirection: Direction = Direction.Down;
+const leftDirection: Direction = Direction.Left;
+const rightDirection: Direction = Direction.Right;
+
+
+console.log(upDirection);
+console.log(downDirection);
+console.log(leftDirection);
+console.log(rightDirection);
+console.log(Direction.Up);
+console.log(Direction.Down);
