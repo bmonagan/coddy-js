df170390351312dadf7ffd73b2fe8364746222d2
bmonagan <bernardmonagan@gmail.com>
2026-02-19 22:30:38 -0500

    creating instances.

---
+++ excercises/creating_instances.ts
@@ -0,0 +1,11 @@
+import { Student } from './student.js';
+
+// TODO: Create an instance of class Student called student1 with name "Sarah" and grade 85
+const student1 = new Student("Sarah", 85);
+
+// TODO: Create an instance of class Student called student2 with name "Mike" and grade 92
+const student2 = new Student("Mike", 92);
+
+// Test code - don't modify
+console.log(student1.name);  // Should output "Sarah"
+console.log(student2.grade); // Should output 92
