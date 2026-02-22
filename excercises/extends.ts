abfa8a4d762bdfa44348ac507caa6071e5c29476
bmonagan <bernardmonagan@gmail.com>
2026-02-22 12:00:00 -0500

    extends

---
+++ excercises/extends.ts
@@ -0,0 +1,14 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  
+  greet() {
+    return `Hello, I'm ${this.name}`;
+  }
+}
+
+// TODO: Create class Student that extends Person and export it
+export class Student extends Person {
+  
+}
