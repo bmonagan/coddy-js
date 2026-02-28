4f4d9c46ca771611c6ddcf3b46fc1cfc7805a39b
bmonagan <bernardmonagan@gmail.com>
2026-02-27 22:25:32 -0500

    static properties

---
+++ excercises/static_properties.js
@@ -0,0 +1,15 @@
+export class MathConstants {
+  // TODO: Add a static property called PI and set its value to 3.14159
+  static PI = 3.14159;
+
+  // TODO: Add a static property called E and set its value to 2.71828
+static E = 2.71828;
+  
+}
+
+=============
+import { MathConstants } from './constants.js';
+
+// Test code - don't modify
+console.log(MathConstants.PI);        // Should output 3.14159
+console.log(MathConstants.E);         // Should output 2.71828
