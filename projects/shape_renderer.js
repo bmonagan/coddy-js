202aa5b0a6c390a6ee6bf297dc02a017b147b8df
bmonagan <bernardmonagan@gmail.com>
2026-03-03 21:42:18 -0500

    feat: implement static property and methods for Shape class to track total shapes created

--- projects/shape_renderer.js
+++ projects/shape_renderer.js
@@ -1,3 +1,25 @@
+export class Shape {
+  // TODO: Add a static property called totalShapes initialized to 0
+  static totalShapes = 0;
+  constructor(color) {
+    this.color = color;
+    // TODO: Increment the counter in the constructor: Shape.totalShapes++;
+    Shape.totalShapes++;
+
+    
+  }
+  
+  describe() {
+    return `A ${this.color} shape`;
+  }
+  
+ // TODO: Add a static method called getTotalCreated() that returns the current count of total shapes   
+ static getTotalCreated() {
+    return Shape.totalShapes;
+ }
+  
+}
+
 import { Shape } from './Shape.js';
 
 export class Circle extends Shape {
@@ -5,32 +27,26 @@ export class Circle extends Shape {
     super(color); 
     this.radius = radius;
   }
+  
   describe() {
     return `${super.describe()} (Circle with radius ${this.radius})`;
   }
+  
   calculateArea() {
     return Math.PI * this.radius * this.radius;
   }
-
 }
 
-export class Shape {
-  constructor(color) {
-    this.color = color;
-  }
-  
-  describe() {
-    return `A ${this.color} shape`;
-  }
-}
 
 import { Shape } from './Shape.js';
 import { Circle } from './Circle.js';
 
 // Tests
-const basicShape = new Shape('red');
-console.log(basicShape.describe()); // "A red shape"
+console.log(`Initial count: ${Shape.getTotalCreated()}`); // Should be 0
 
-const myCircle = new Circle('blue', 10);
-console.log(myCircle.describe()); // Should output: "A blue shape (Circle with radius 10)"
-console.log(myCircle.calculateArea()); // Should output: "314.1592653589793"
+const shape1 = new Shape('red');
+const shape2 = new Shape('green');
+const circle1 = new Circle('blue', 5);
+const circle2 = new Circle('yellow', 10);
+
+console.log(`After creating 4 shapes: ${Shape.getTotalCreated()}`); // Should be 4
