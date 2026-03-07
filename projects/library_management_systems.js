ccb88773424acf701e0eec991e739d6e291f1c5c
bmonagan <bernardmonagan@gmail.com>
2026-03-06 22:14:02 -0500

    refactor: improve Book class constructor validation for author instance

--- projects/library_management_systems.js
+++ projects/library_management_systems.js
@@ -1,32 +1,49 @@
-// TODO: Add import for Author class here
-import { Author } from "./Author.js";
-
-// TODO: Create and export the Book class 
-export class Book {
-    #title;
-    #author;
-    #isCheckedOut = false;
-    constructor(title,author) {
-        this.#title = title;
-        this.#author = author;
-    }
-    get title() {
-        return this.#title;
-    }
-    get authorInfo() {
-        return this.#author.name;
-    }
-}
-
 import { Author } from './Author.js';
 
-// TODO:  Import the Book class
+export class Book {
+  #title;
+  #author;  // Composition: Book HAS-AN Author
+  #isCheckedOut = false;
+ 
+  constructor(title, author) {
+    this.#title = title;
+    
+    // TODO: Check if the author is valid (an instance of Author class) 
+    // use author instanceof Author
+    if (author instanceof Author) {
+        this.#author = author;
+    }
+    else {
+        console.log("Invalid author: must be Author instance");
+        this.#author = null;
+    }
+    // TODO: If the author is valid, set this.#author to author
 
-import { Book } from "./Book.js";
+    // TODO: Otherwise (author is invalid), log exactly: "Invalid author: must be Author instance"
+    
+    // TODO: Set this.#author = null
+  }
+  
+  // Getter for title (read-only)
+  get title() {
+    return this.#title;
+  }
+  
+  // Getter for author info
+  get authorInfo() {
+    return this.#author.name;
+  }
+}
+===
+import { Author } from './Author.js';
+import { Book } from './Book.js';
 
 // Tests
-const author = new Author('J.K. Rowling');
-const book = new Book('Harry Potter', author);
+const rowling = new Author('J.K. Rowling');
 
-console.log('Book title:', book.title); // Should output: "Book title: Harry Potter"
-console.log('Author:', book.authorInfo); // Should output: "Author: J.K. Rowling"
+// Valid case (should work silently)
+const validBook = new Book('Harry Potter', rowling);
+console.log('Author:', validBook.authorInfo); // Should output: "Author: J.K. Rowling"
+
+// Invalid cases (should log message but still create book)
+const invalid1 = new Book('Fake Book', 'String Author');
