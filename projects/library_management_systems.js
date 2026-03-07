6b7402ea5b47a62e0053013bfa61d2450821b997
bmonagan <bernardmonagan@gmail.com>
2026-03-06 22:12:30 -0500

    refactor: update imports and implement Book class with author information

--- projects/library_management_systems.js
+++ projects/library_management_systems.js
@@ -1,14 +1,32 @@
-// TODO: import the Author class here
-import Author from "./Author.js"
+// TODO: Add import for Author class here
+import { Author } from "./Author.js";
 
-// Tests
-const author = new Author('J.K. Rowling');
-console.log(author.name);        // "J.K. Rowling"
-===
-// TODO: Create an Author class and export it
-export default class Author {
-    constructor(name) {
-        this.name = name;
+// TODO: Create and export the Book class 
+export class Book {
+    #title;
+    #author;
+    #isCheckedOut = false;
+    constructor(title,author) {
+        this.#title = title;
+        this.#author = author;
+    }
+    get title() {
+        return this.#title;
+    }
+    get authorInfo() {
+        return this.#author.name;
     }
 }
 
+import { Author } from './Author.js';
+
+// TODO:  Import the Book class
+
+import { Book } from "./Book.js";
+
+// Tests
+const author = new Author('J.K. Rowling');
+const book = new Book('Harry Potter', author);
+
+console.log('Book title:', book.title); // Should output: "Book title: Harry Potter"
+console.log('Author:', book.authorInfo); // Should output: "Author: J.K. Rowling"
